a607b91e7899e6543672bb91afd3b047192f0175
alas42 <44909605+alas42@users.noreply.github.com>
2021-01-17 21:34:59 +0100

    refleshed version - server side functionning with node - js not working anymore - another time, another ... (#5)
    Co-authored-by: unknown <alexandre.vogt7@etu.univ-lorraine.fr>

---
+++ public/script.js
@@ -0,0 +1,28 @@
+let gameRoom;
+let deadPlayerCh;
+let myClientId;
+let myChannel;
+let gameOn = false;
+let players = {};
+let totalPlayers = 0;
+let latestShipPosition;
+let bulletThatShotMe;
+let bulletThatShotSomeone;
+let bulletOutOfBounds = "";
+let amIalive = false;
+let game;
+
+const BASE_SERVER_URL = "http://localhost:5000";
+const myNickname = localStorage.getItem("nickname");
+
+const realtime = Ably.Realtime({
+    authUrl: BASE_SERVER_URL + "/auth",
+  });
+  
+  realtime.connection.once("connected", () => {
+    myClientId = realtime.auth.clientId;
+    gameRoom = realtime.channels.get("game-room");
+    deadPlayerCh = realtime.channels.get("dead-player");
+    myChannel = realtime.channels.get("clientChannel-" + myClientId);
+    gameRoom.presence.enter(myNickname);
+  });
